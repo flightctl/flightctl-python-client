5316eee4294e1a0e11c1b64f2cdb7b123c32e02b
Siddarth Royapally <60885840+SiddarthR56@users.noreply.github.com>
2025-03-04 02:56:45 -0500

    Update certificate_signing_request_spec.py

--- flightctl/models/certificate_signing_request_spec.py
+++ flightctl/models/certificate_signing_request_spec.py
@@ -89,8 +89,8 @@ class CertificateSigningRequestSpec(BaseModel):
         _obj = cls.model_validate({
             "expirationSeconds": obj.get("expirationSeconds"),
             "extra": obj.get("extra"),
-            "request": obj.get("request"),
-            "signerName": obj.get("signerName"),
+            "request": obj.get("request") if isinstance(obj.get("request"), (bytes, str)) else b"",
+            "signerName": obj.get("signerName") or "",
             "uid": obj.get("uid"),
             "usages": obj.get("usages"),
             "username": obj.get("username")
